54cb3ac6030ed1a2769bf74e400794848ac685cc
Sclarke5 <sclarke5555@gmail.com>
2018-03-28 14:00:58 -0400

    finished product class, begins methods in cart class

---
+++ cart.rb
@@ -0,0 +1,32 @@
+require_relative ('list')
+
+class Cart
+
+  @@products = []
+
+  def self.add_product
+
+  end
+
+  def self.remove_product
+
+  end
+
+  def self.total_before_tax
+
+  end
+
+  def self.total_after_tax
+
+  end
+
+
+
+# Add product
+# delete product
+# total before tax
+# total after tax
+# display product class variable
+
+
+end
---
+++ product.rb
@@ -0,0 +1,38 @@
+class Product
+
+  @@products = []
+
+  def initialize(name, base_price)
+    @name       = name
+    @base_price = base_price
+    @tax_rate   = 0.20
+  end
+
+  def self.create(name, base_price)
+    new_product = Product.new(name, base_price)
+    @@products.push(new_product)
+    return new_product
+  end
+
+  def self.all
+    @@products
+  end
+
+  def base_price
+    @base_price
+  end
+
+  def total_price
+    @base_price + (@base_price * @tax_rate)
+  end
+end
+
+product1 = Product.create('banana', 2.50)
+product2 = Product.create('apple', 2.00)
+product3 = Product.create('yogurt', 28.00)
+
+puts Product.all.inspect
+
+p product3.total_price
+
+p
